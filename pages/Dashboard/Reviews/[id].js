581d511a45e329cebc86472633294274b8c6a698
Abbas Abdallah <abbasab13@outlook.com>
2023-01-22 23:51:03 +0200

    updated

--- pages/Dashboard/Reviews/[id].js
+++ pages/Dashboard/Reviews/[id].js
@@ -8,13 +8,16 @@ const Id = () => {
     const router = useRouter();
 
     const [review, setReview] = useState([]);
+    const [oldReview, setOldReview] = useState([]);
+    
     
     
     
     useEffect(() => {
       const query = `*[_type == 'reviewsToDashboard' && _id == '${router.query.id}']`;
-      
+      const query2 = `*[_type == 'reviews' && _id == '${router.query.id}']`
       client.fetch(query).then(data => setReview(data[0]));
+      client.fetch(query2).then(data => setOldReview(data[0]));
     }, []);
 
     const acceptReview = () => {
@@ -34,48 +37,86 @@ const Id = () => {
         client.delete({query: `*[_type == "reviewsToDashboard" && _id == "${review._id}"]`}).then(() => router.back());
     }
 
+    const deleteOldFeedback = () => {
+      client.delete({query: `*[_type == "reviews" && _id == "${oldReview._id}"]`}).then(() => router.back())
+    }
+
     
     
 
   return (
+    
   <div className="z-[999] absolute top-[0rem] h-screen w-screen bg-[#272935] p-2 flex flex-col justify-around">
     <div className="mt-24">
         <BiArrowBack size='5rem' color='#fff' onClick={() => router.back()}/>
     </div>
+    {
+      review ? (
+       <>
+       <div className="mt-4">
+          <label className="text-[#86DFE4] text-4xl">Full Name:</label>
+          <p className="text-white text-2xl ml-4">{review.fullname}</p>
+        </div>
+        
+        <div className="mt-4">
+          <label className="text-[#86DFE4] text-4xl">Gender:</label>
+          <p className="text-white text-2xl ml-4"  >{review?.gender}</p>
+        </div>
+
+        <div className="mt-4">
+          <label className="text-[#86DFE4] text-4xl">Star Rating:</label>
+          <p className="text-white text-2xl ml-4"  >{(review?.score)/20} Stars</p>
+        </div>
+
+        <div className="mt-4">
+          <label className="text-[#86DFE4] text-4xl">Feedback:</label>
+          <p className="text-white text-2xl ml-4"  >{review?.feedback}</p>
+        </div>
+
+        <div className="mt-4">
+          <label className="text-[#86DFE4] text-4xl">Reason:</label>
+          <p className="text-white text-2xl ml-4"  >{review?.reason}</p>
+        </div>
+        <div className="flex justify-center w-full mt-4 gap-2">
+        <button className="bg-[#007C14] p-2 text-white rounded-md" onClick={() => acceptReview()}>Accept</button>
+        <button className="bg-[#790505] p-2 text-white rounded-md" onClick={() => rejectReview()} >Reject</button>
+      </div>
+        </>
+      ):
+      (
+<>
+       <div className="mt-4">
+          <label className="text-[#86DFE4] text-4xl">Full Name:</label>
+          <p className="text-white text-2xl ml-4">{oldReview?.fullname}</p>
+        </div>
+        
+        <div className="mt-4">
+          <label className="text-[#86DFE4] text-4xl">Gender:</label>
+          <p className="text-white text-2xl ml-4"  >{oldReview?.gender}</p>
+        </div>
+
+        <div className="mt-4">
+          <label className="text-[#86DFE4] text-4xl">Star Rating:</label>
+          <p className="text-white text-2xl ml-4"  >{(oldReview?.score)/20} Stars</p>
+        </div>
+
+        <div className="mt-4">
+          <label className="text-[#86DFE4] text-4xl">Feedback:</label>
+          <p className="text-white text-2xl ml-4"  >{oldReview?.feedback}</p>
+        </div>
+
+        <div className="mt-4">
+          <label className="text-[#86DFE4] text-4xl">Reason:</label>
+          <p className="text-white text-2xl ml-4"  >{oldReview?.reason}</p>
+        </div>
+        <div>
+          <button className="bg-[#790505] p-2 text-white rounded-md" onClick={()=>  deleteOldFeedback()}>Delete</button>
+        </div>
+        </>
+      )
+    }
     
-    <div className="mt-4">
-      <label className="text-[#86DFE4] text-4xl">Full Name:</label>
-      <p className="text-white text-2xl ml-4">{review.fullname}</p>
-    </div>
-    
-    <div className="mt-4">
-      <label className="text-[#86DFE4] text-4xl">Gender:</label>
-      <p className="text-white text-2xl ml-4"  >{review.gender}</p>
-    </div>
-
-    <div className="mt-4">
-      <label className="text-[#86DFE4] text-4xl">Star Rating:</label>
-      <p className="text-white text-2xl ml-4"  >{(review.score)/20} Stars</p>
-    </div>
-
-    <div className="mt-4">
-      <label className="text-[#86DFE4] text-4xl">Feedback:</label>
-      <p className="text-white text-2xl ml-4"  >{review.feedback}</p>
-    </div>
-
-    <div className="mt-4">
-      <label className="text-[#86DFE4] text-4xl">Reason:</label>
-      <p className="text-white text-2xl ml-4"  >{review.reason}</p>
-    </div>
-    
-
-    <div className="flex justify-center w-full mt-4 gap-2">
-      <button className="bg-[#007C14] p-2 text-white rounded-md" onClick={() => acceptReview()}>Accept</button>
-      <button className="bg-[#790505] p-2 text-white rounded-md" onClick={() => rejectReview()} >Reject</button>
-    </div>
-
-
-  </div>
+ </div>
   )
 };
 
