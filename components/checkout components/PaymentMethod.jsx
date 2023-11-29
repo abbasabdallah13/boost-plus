88e954aca540c52979de2f0f8de70e4252109574
Abbas Abdallah <abbasab13@outlook.com>
2023-11-29 20:16:03 +0200

    debugging stripe 4

--- components/checkout components/PaymentMethod.jsx
+++ components/checkout components/PaymentMethod.jsx
@@ -53,6 +53,8 @@ function PaymentMethod({ paymentMethodModal }) {
         })
         if(response.statusCode === 500) return;
         
+        const data = await response.json();
+        console.log(data)
         toast.loading('Redirecting..');
     
         stripe.redirectToCheckout({sessionId: data.id});
