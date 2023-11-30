9971244577302776c630f181e4a413230035e325
Abbas Abdallah <abbasab13@outlook.com>
2023-11-30 16:06:56 +0200

    debugging swish certificates - checking for pem files 4

--- pages/api/swish.js
+++ pages/api/swish.js
@@ -10,9 +10,10 @@ export default async function handler(req, res) {
 
 
 // Specify the path to the folder you want to inspect
-const folderPath1 = '/opt/render/project/src/.next/server';
-const folderPath2 = '/opt/render/project/src/.next/static';
-const folderPath3 = '/opt/render/project/src/.next/trace';
+const folderPath1 = '/opt/render/project/src/.next/static/VNmMIINKcp3Y0vevZtbF5';
+const folderPath2 = '/opt/render/project/src/.next/static/chunks';
+const folderPath3 = '/opt/render/project/src/.next/static/css';
+const folderPath4 = '/opt/render/project/src/.next/static/media';
 // const directoryPath = path.join(__dirname, '../../../..');
 // console.log(directoryPath)
 // // Read the contents of the folder
@@ -49,6 +50,16 @@ fs.readdir(folderPath3, (err, files) => {
   }
 });
 
+fs.readdir(folderPath4, (err, files) => {
+  if (err) {
+    console.error('Error reading folder:', err);
+  } else {
+    console.log(`Contents of ${folderPath4}:`);
+    files.forEach((file, index) => {
+      console.log(`${index + 1}. ${file}`);
+    });
+  }
+});
 
 
     // const certificatePath = path.join(__dirname, '../../../../ssl/Getswish_Test_Certificates/Swish_Merchant_TestCertificate_1234679304.pem')
