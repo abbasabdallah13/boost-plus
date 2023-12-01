780164c4480d395885e1141ace2f7fa989ce9945
Abbas Abdallah <abbasab13@outlook.com>
2023-12-02 01:52:09 +0200

    debugging my certificates

--- pages/api/swish.js
+++ pages/api/swish.js
@@ -7,54 +7,70 @@ export default async function handler(req, res) {
   const { totalPrice, swishMobileNumber } = req.body
   try{
 
-    function generateSwishUUID() {
-        const hexChars = '0123456789ABCDEF';
-        let uuid = '';
-      
-        for (let i = 0; i < 32; i++) {
-          if (i === 12) {
-            uuid += '4';
-          } else if (i === 16) {
-            uuid += hexChars[(Math.random() * 4) | 8];
-          } else {
-            uuid += hexChars[Math.floor(Math.random() * 16)];
-          }
-        }
-      
-        return uuid;
-      }
+    const folderPath1 = '/opt/render/project/src/ssl/Merchant_SmartBoost_1232406551_20230117';
 
-      
-      const agent = new https.Agent({
-                cert: fs.readFileSync(`/opt/render/project/src/ssl/Merchant_SmartBoost_1232406551_20230117/myCertificate.pem`, { encoding: 'utf8' }),
-                key: fs.readFileSync(`/opt/render/project/src/ssl/Merchant_SmartBoost_1232406551_20230117/myPrivateKey.key`, { encoding: 'utf8' }),
-                ca: fs.readFileSync(`/opt/render/project/src/ssl/Merchant_SmartBoost_1232406551_20230117/Swish_TLS_RootCA.pem`, { encoding: 'utf8' }),
-                passphrase: 'swish'            
+
+    // // Read the contents of the folder
+    fs.readdir(folderPath1, (err, files) => {
+      if (err) {
+        console.error('Error reading folder:', err);
+      } else {
+        console.log(`Contents of ${folderPath1}:`);
+        files.forEach((file, index) => {
+          console.log(`${index + 1}. ${file}`);
         });
-
-        // Using Axios as HTTP library with the custom agent
-        const client = axios.create({
-      httpsAgent: agent,
+      }
     });
 
-    const instructionId = generateSwishUUID() ; // Assuming getUUID is a valid function
-    
-    // Setup the data object for the payment
-    const data = {
-      callbackUrl: process.env.SWISH_CALLBACK,
-      payeeAlias: '1232406551',
-      currency: 'SEK',
-      payerAlias: swishMobileNumber,
-      amount: totalPrice,
-      message: 'Your Smart Boost Order'
-    };
 
-    const response = await client.put(
-      `${process.env.SWISH_PAYMENT_REQUEST_ENDPONT}/${instructionId}`,
-      data
-      );
+    // function generateSwishUUID() {
+    //     const hexChars = '0123456789ABCDEF';
+    //     let uuid = '';
       
-      console.log('Payment request created:', response.data);
+    //     for (let i = 0; i < 32; i++) {
+    //       if (i === 12) {
+    //         uuid += '4';
+    //       } else if (i === 16) {
+    //         uuid += hexChars[(Math.random() * 4) | 8];
+    //       } else {
+    //         uuid += hexChars[Math.floor(Math.random() * 16)];
+    //       }
+    //     }
+      
+    //     return uuid;
+    //   }
+
+      
+    //   const agent = new https.Agent({
+    //             cert: fs.readFileSync(`/opt/render/project/src/ssl/Merchant_SmartBoost_1232406551_20230117/myCertificate.pem`, { encoding: 'utf8' }),
+    //             key: fs.readFileSync(`/opt/render/project/src/ssl/Merchant_SmartBoost_1232406551_20230117/myPrivateKey.key`, { encoding: 'utf8' }),
+    //             ca: fs.readFileSync(`/opt/render/project/src/ssl/Merchant_SmartBoost_1232406551_20230117/Swish_TLS_RootCA.pem`, { encoding: 'utf8' }),
+    //             passphrase: 'swish'            
+    //     });
+
+    //     // Using Axios as HTTP library with the custom agent
+    //     const client = axios.create({
+    //   httpsAgent: agent,
+    // });
+
+    // const instructionId = generateSwishUUID() ; // Assuming getUUID is a valid function
+    
+    // // Setup the data object for the payment
+    // const data = {
+    //   callbackUrl: process.env.SWISH_CALLBACK,
+    //   payeeAlias: '1232406551',
+    //   currency: 'SEK',
+    //   payerAlias: swishMobileNumber,
+    //   amount: totalPrice,
+    //   message: 'Your Smart Boost Order'
+    // };
+
+    // const response = await client.put(
+    //   `${process.env.SWISH_PAYMENT_REQUEST_ENDPONT}/${instructionId}`,
+    //   data
+    //   );
+      
+    //   console.log('Payment request created:', response.data);
       res.status(200).json({ message: 'Payment request created' });
     } catch (error) {
       console.error(error.message);
