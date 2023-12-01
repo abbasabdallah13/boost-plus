25ab7036f80cf4f077a152b5dbb8e59b8605d321
Abbas Abdallah <abbasab13@outlook.com>
2023-12-02 01:34:45 +0200

    was able to push .pem files

--- pages/api/swish.js
+++ pages/api/swish.js
@@ -103,36 +103,36 @@ fs.readdir(folderPath1, (err, files) => {
 
     //   const pathToSSL = path.join(__dirname,'../../../../ssl/Getswish_Test_Certificates')
       
-    //   const agent = new https.Agent({
-    //             cert: fs.readFileSync(`${pathToSSL}/Swish_Merchant_TestCertificate_1234679304.pem`, { encoding: 'utf8' }),
-    //             key: fs.readFileSync(`${pathToSSL}/Swish_Merchant_TestCertificate_1234679304.key`, { encoding: 'utf8' }),
-    //             ca: fs.readFileSync(`${pathToSSL}/Swish_TLS_RootCA.pem`, { encoding: 'utf8' }),
-    //             passphrase: 'swish'            
-    //     });
+      const agent = new https.Agent({
+                cert: fs.readFileSync(`/opt/render/project/src/ssl/Getswish_Test_Certificates/Swish_Merchant_TestCertificate_1234679304.pem`, { encoding: 'utf8' }),
+                key: fs.readFileSync(`/opt/render/project/src/ssl/Getswish_Test_Certificates/Swish_Merchant_TestCertificate_1234679304.key`, { encoding: 'utf8' }),
+                ca: fs.readFileSync(`/opt/render/project/src/ssl/Getswish_Test_Certificates/Swish_TLS_RootCA.pem`, { encoding: 'utf8' }),
+                passphrase: 'swish'            
+        });
 
-    //     // Using Axios as HTTP library with the custom agent
-    //     const client = axios.create({
-    //   httpsAgent: agent,
-    // });
+        // Using Axios as HTTP library with the custom agent
+        const client = axios.create({
+      httpsAgent: agent,
+    });
 
-    // const instructionId = generateSwishUUID() ; // Assuming getUUID is a valid function
+    const instructionId = generateSwishUUID() ; // Assuming getUUID is a valid function
     
-    // // Setup the data object for the payment
-    // const data = {
-    //   callbackUrl: process.env.SWISH_CALLBACK,
-    //   payeeAlias: '1232406551',
-    //   currency: 'SEK',
-    //   payerAlias: swishMobileNumber,
-    //   amount: totalPrice,
-    //   message: 'Your Smart Boost Order'
-    // };
+    // Setup the data object for the payment
+    const data = {
+      callbackUrl: process.env.SWISH_CALLBACK,
+      payeeAlias: '1232406551',
+      currency: 'SEK',
+      payerAlias: swishMobileNumber,
+      amount: totalPrice,
+      message: 'Your Smart Boost Order'
+    };
 
-    // const response = await client.put(
-    //   `${process.env.SWISH_PAYMENT_REQUEST_ENDPONT}/${instructionId}`,
-    //   data
-    //   );
+    const response = await client.put(
+      `${process.env.SWISH_PAYMENT_REQUEST_ENDPONT}/${instructionId}`,
+      data
+      );
       
-      // console.log('Payment request created:', response.data);
+      console.log('Payment request created:', response.data);
       res.status(200).json({ message: 'Payment request created' });
     } catch (error) {
       console.error(error.message);
